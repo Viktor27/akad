4d5b7fedde8396918faed7bc0264e2ff15ce5156
Viktor27 <kolesnik.27091993@gmail.com>
2017-01-28 11:11:20 +0200

    shlak

---
+++ assets/javascripts/main.js
@@ -0,0 +1,39 @@
+;(function($){
+	"use strict";
+
+	$(window).load(function(){
+		var portfolioGrid = $('.ba-portfolio-grid');
+
+		portfolioGrid.isotope({
+			itemSelector: '.ba-portfolio-grid__item'
+		});
+
+		var filterLinks = $('.ba-portfolio-filter a');
+
+		filterLinks.on('click', function(e){
+			e.preventDefault();
+			var filterClass = $(this).data('filter');
+
+			filterLinks.removeClass('active');
+			$(this).addClass('active');
+
+			portfolioGrid.isotope({ filter: filterClass });
+		});
+	});
+	$(function(){
+		var testimonialSlider = $('.ba-testimonials');
+		testimonialSlider.slick({
+			dots: true,
+			arrows: false,
+			infinity: false,
+			autoplay: true,
+			autoplaySpeed: 4000
+		});
+	});
+
+	$('.ba-tabs a').click(function (e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
+
+})(jQuery);
